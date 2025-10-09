d63a513ffec616cab5c558b1934bdaebc840aa02
yashika29092006 <yashikaa2529@gmail.com>
2025-10-09 14:04:05 +0530

    added a new  for loop file

---
+++ task/loop_demo.py
@@ -0,0 +1,6 @@
+# print 1 to n using for loop
+
+n = int(input("Enter the number:"))
+a=1
+for i in range(a,n+1):
+    print(i)
--- task/variables.py
+++ task/variables.py
@@ -1,6 +1,8 @@
+# adding to two numbers
 a = 10
 b = 20 
 print("Sum = ",a+b)
 
 x = "Hello world!"
 print(x)
+
